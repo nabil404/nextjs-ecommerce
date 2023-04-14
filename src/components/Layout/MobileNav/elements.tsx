54508fed4e47b688def7a204404b5e112b283ab3
Nabil Shahid <nabilmd901@gmail.com>
2023-04-14 17:23:50 +0600

    add auth buttons in sidebar navigation

--- src/components/Layout/MobileNav/elements.tsx
+++ src/components/Layout/MobileNav/elements.tsx
@@ -1,4 +1,5 @@
-import {styled, IconButton} from "@mui/material";
+import {styled, IconButton, Button, Grid} from "@mui/material";
+import {green, blue} from "@mui/material/colors";
 
 export const StyledIconButton = styled(IconButton)`
   && {
@@ -10,5 +11,36 @@ export const StyledIconButton = styled(IconButton)`
       font-size: 25px;
     }
   }
+`
 
+export const AuthButtonContainer = styled(Grid)`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 0 4rem;
+  margin-top: 10px;
+`
+
+export const StyledLoginButton = styled(Button)`
+  && {
+    background: ${green[300]};
+
+    &:hover {
+      background: ${green[400]};
+    }
+  }
+`
+
+export const StyledRegisterButton = styled(Button)`
+  && {
+    background: ${blue[400]};
+
+    &:hover {
+      background: ${blue[500]};
+    }
+  }
+`
+
+export const NavigationContainer = styled(Grid)`
+  //padding: 0 1rem;
 `
