677526a012e94b82a81de088821d246992c53282
Nabil Shahid <nabilmd901@gmail.com>
2023-04-15 17:44:04 +0600

    add search bar in middle header

---
+++ src/components/Layout/CustomerService/CustomerService.tsx
@@ -0,0 +1,13 @@
+import {Stack, Typography} from "@mui/material";
+import {StyledContainer, StyledHeader, StyledNumber} from "@/components/Layout/CustomerService/elements";
+
+function CustomerService() {
+  return (
+    <StyledContainer>
+      <StyledHeader variant={'caption'}>Customer Service</StyledHeader>
+      <StyledNumber variant={'subtitle2'}>+880-1822644891</StyledNumber>
+    </StyledContainer>
+  )
+}
+
+export default CustomerService
--- src/components/Layout/SiteHeader/SiteHeaderMiddle/elements.tsx
+++ src/components/Layout/SiteHeader/SiteHeaderMiddle/elements.tsx
@@ -1,8 +1,9 @@
 import {styled, Container} from "@mui/material";
 
 export const StyledContainer = styled(Container)`
-  height: 6.5rem;
+  height: 6rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  gap: 5rem;
 `
